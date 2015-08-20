4e9a6112d987b454de6ede6e649edba2b9206acc
Andrew Fecheyr <andrew@bedesign.be>
2015-08-20 21:32:29 +0200

    Match JSON root element for EmberData

--- api/web/views/post_view.ex
+++ api/web/views/post_view.ex
@@ -2,11 +2,11 @@ defmodule Peepblog.PostView do
   use Peepblog.Web, :view
 
   def render("index.json", %{posts: posts}) do
-    %{data: render_many(posts, Peepblog.PostView, "post.json")}
+    %{posts: render_many(posts, Peepblog.PostView, "post.json")}
   end
 
   def render("show.json", %{post: post}) do
-    %{data: render_one(post, Peepblog.PostView, "post.json")}
+    %{post: render_one(post, Peepblog.PostView, "post.json")}
   end
 
   def render("post.json", %{post: post}) do
